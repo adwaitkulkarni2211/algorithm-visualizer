189f01e627b3655796137d7db144bb105f6819d1
adwait kulkarni <adwaitkulkarni2211@gmail.com>
2022-03-04 08:46:14 +0530

    colored comparing bars

--- src/App.js
+++ src/App.js
@@ -4,44 +4,42 @@ import "./App.css";
 function App() {
   const [arr, setArr] = useState([]);
   const [masterArr, setMasterArr] = useState([]);
+  const [delay, setDelay] = useState(5);
+  const [activeBars, setActiveBars] = useState([]);
 
   useEffect(() => {
     generateRandomArray();
   }, []);
 
-  useEffect(() => {
-    console.log("masterArr after re-render:", masterArr);
-  })
-
   const generateRandomArray = () => {
     const tempArr = [];
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 120; i++) {
       tempArr.push({
         num: Math.floor(Math.random() * 1000),
         idx: i,
       });
     }
-    // for(let i=500, j=0; i>=100; i-=100, j++) {
-    //   tempArr.push({num: i, idx: j});
-    // }
     setArr([...tempArr]);
     setMasterArr([...tempArr]);
-    //console.log("from generate random arr: ", arr);
   };
 
-  function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  function timeout() {
+    return new Promise(resolve => setTimeout(resolve, delay));
   }
 
   const mergeTwoSortedLists = async (arr1, arr2, tempMasterArr) => {
     let mergedArr = [];
-    await timeout(10)
-    console.log("arr1: ", arr1, " arr2: ", arr2);    
+
+    await timeout()  
+
     tempMasterArr = [...tempMasterArr.slice(0, Math.min.apply(Math, arr1.map(o => o.idx))), ...arr1, ...arr2, ...arr.slice(Math.max.apply(Math, arr2.map(o => o.idx)) + 1)];
     setMasterArr([...tempMasterArr]);
 
     let i = 0, j = 0;
     while(i < arr1.length && j < arr2.length) {
+      await timeout()
+      setActiveBars([arr1[i], arr2[j]]);
+
       if(arr2[j].num < arr1[i].num) {
           mergedArr.push(arr2[j]);
           j++;
@@ -50,24 +48,38 @@ function App() {
           i++;
       }
 
-      await timeout(10)      
+      await timeout()
+
       tempMasterArr = [...tempMasterArr.slice(0, Math.min.apply(Math, arr1.map(o => o.idx))), ...mergedArr, ...arr1.slice(i), ...arr2.slice(j), ...arr.slice(Math.max.apply(Math, arr2.map(o => o.idx)) + 1)];
       setMasterArr([...tempMasterArr]);
+      setActiveBars([]);
     }
 
     while(i < arr1.length) {
+      await timeout();
+      setActiveBars([arr1[i]]);
+
       mergedArr.push(arr1[i]);
       i++;
-      await timeout(10)
+
+      await timeout()
+
       tempMasterArr = [...tempMasterArr.slice(0, Math.min.apply(Math, arr1.map(o => o.idx))), ...mergedArr, ...arr1.slice(i), ...arr.slice(Math.max.apply(Math, arr2.map(o => o.idx)) + 1)]
       setMasterArr([...tempMasterArr]);
+      setActiveBars([])
     }
     while(j < arr2.length) {
+      await timeout();
+      setActiveBars([arr2[j]]);
+
       mergedArr.push(arr2[j]);
       j++;
-      await timeout(10)      
+
+      await timeout()  
+
       tempMasterArr = [...tempMasterArr.slice(0, Math.min.apply(Math, arr1.map(o => o.idx))), ...mergedArr, ...arr2.slice(j), ...arr.slice(Math.max.apply(Math, arr2.map(o => o.idx)) + 1)]
       setMasterArr([...tempMasterArr]);
+      setActiveBars([])
     }
 
     return {mergedArr: mergedArr, tempMasterArr: tempMasterArr};
@@ -96,12 +108,8 @@ function App() {
       <button
         className="sort-btn"
         onClick={async () => {
-          console.log("before: ", arr);
           let tempMasterArr = [...masterArr];
           const ans = await mergeSort(arr, 0, arr.length - 1, tempMasterArr);
-          setArr([...ans])
-          console.log("ans: ", ans);
-          console.log("arr: ", arr);
         }}
       >
         Merge Sort
@@ -116,7 +124,7 @@ function App() {
                 height: `${((num.num * 50) / 1000) * 5}px`,
                 width: `2px`,
                 margin: `2px`,
-                backgroundColor: "blue",
+                backgroundColor: activeBars.find(bar => bar.idx == num.idx) ? "black" : "cyan",
               }}
             >
             </div>
